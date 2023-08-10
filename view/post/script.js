297750648b29b755bcc06cf6142b8f2cddcba91b
Your Name <you@example.com>
2023-08-10 18:07:47 -0300

    post

--- view/post/script.js
+++ view/post/script.js
@@ -1,43 +1,47 @@
-let btn = document.querySelector("button")
-let form = document.querySelector("form")
 
-btn.addEventListener('click', enviarDados)
-
-function enviarDados(event){
+export default function enviarDados(event) {
     event.preventDefault();
     // selecionando objetos html
+    let form = document.querySelector("form")
+
     let inputId = form.idUser
     let inputNome = form.nomeUser
     let inputPreco = form.telUser
-    
+
     // obtendo o conteúdo de texto
     let id = inputId.value
     let nome = inputNome.value
     let preco = inputPreco.value
 
-    let produto = {
-        id: id,
-        nome: nome,
-        preco: preco
+    if (id == "" || nome == "" || preco == "") {
+        console.log("vazio")
+    } else {
+        let produto = {
+            id: id,
+            nome: nome,
+            preco: preco
+        }
+        form.reset()
+        console.log(produto)
+        incluirDados(produto)
     }
-    form.reset()
-    
-    incluirDados(produto)
+
 }
 
-function incluirDados(produto){
+function incluirDados(produto) {
     let url = "../../controller/POST_produto.php"
-    fetch(url,{
+    fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-          },
-          body: JSON.stringify(produto)
+        },
+        body: JSON.stringify(produto)
     })
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (response) {
-        console.log(response)
-    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (response) {
+            console.log(response)
+            setTimeout(location.reload(), 5000)
+        })
 }
