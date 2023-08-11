ee874305d81d808273ec8d8c1e67bc08570ff559
Your Name <camila.silva1449@etec.sp.gov.br>
2023-08-11 15:22:58 -0300

    post

--- controller/POST_produto.php
+++ controller/POST_produto.php
@@ -6,15 +6,18 @@
     // recebendo os dados do front
     $dados = json_decode(file_get_contents('php://input'));
     $usuario = new Produto($dados->id, $dados->nome, $dados->tel, $dados->email, $dados->cep, $dados->cpf);
+
+
    
     // lendo o arquivo de dados
     $usuarios = json_decode(realizarLeitura());
-    array_push($usuarios, $usuario);
+    array_push($usuarios, $usuario);    
     
+
+            $json = json_encode($usuarios);
+            escrever($json);
+            echo json_encode($usuario);
         
-    $json = json_encode($usuarios);
-    escrever($json);
-
-
-    echo json_encode($usuario);
+        
+    
 ?>
