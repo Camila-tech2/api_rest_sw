f1a22b06112884703a942c8aa53855fbbaf6b8ca
oculosdanilo <oculosdanilo@gmail.com>
2023-08-11 21:18:58 -0300

    08/11/2023 21:18:58

--- controller/DELETE_produto.php
+++ controller/DELETE_produto.php
@@ -1,31 +1,35 @@
 <?php
-    
-    require_once './ler.php';
-    require_once './escrever.php';
-    
-    $dados = json_decode(file_get_contents('php://input'));
 
-    $id = $dados->id;
-    
-    $usuarios = json_decode(realizarLeitura());
-    
-    foreach($usuarios as $u) {
-        if($u->id == $id){
-            unset($usuarios[$id]);
-            break;
-        }
+require_once './ler.php';
+require_once './escrever.php';
+
+$dados = json_decode(file_get_contents('php://input'));
+
+$id = $dados->id;
+
+$usuarios = json_decode(realizarLeitura());
+
+foreach ($usuarios as $u)
+{
+    if ($u->id == $id)
+    {
+        unset($usuarios[$id]);
+        break;
     }
-    $lista = [];
-    foreach ($usuarios as $user) {
-       if (!$user == null) {
-            array_push($lista, $user);
-       }
+}
+$lista = [];
+foreach ($usuarios as $user)
+{
+    if (!$user == null)
+    {
+        array_push($lista, $user);
     }
+}
 
 
-    $json = json_encode($lista);
-    escrever($json);
+$json = json_encode($lista);
+escrever($json);
 
-    echo $json;
+echo $json;
 
 ?>
--- controller/POST_produto.php
+++ controller/POST_produto.php
@@ -1,23 +1,30 @@
 <?php
-    require_once '../model/Produto.php';
-    require_once './ler.php';
-    require_once './escrever.php';
-   
-    // recebendo os dados do front
-    $dados = json_decode(file_get_contents('php://input'));
-    $usuario = new Produto($dados->id, $dados->nome, $dados->tel, $dados->email, $dados->cep, $dados->cpf);
+require_once '../model/Produto.php';
+require_once './ler.php';
+require_once './escrever.php';
+
+// recebendo os dados do front
+$dados = json_decode(file_get_contents('php://input'));
+$usuarios = json_decode(realizarLeitura());
+$postar = true;
+for ($i = 0; $i < count($usuarios); $i++)
+{
+  if ($usuarios[$i]->id == $dados->id)
+  {
+    $postar = false;
+    break;
+  }
+}
+$usuario = new Produto($dados->id, $dados->nome, $dados->tel, $dados->email, $dados->cep, $dados->cpf);
+
+// lendo o arquivo de dados
+array_push($usuarios, $usuario);
 
 
-   
-    // lendo o arquivo de dados
-    $usuarios = json_decode(realizarLeitura());
-    array_push($usuarios, $usuario);    
-    
+$json = json_encode($usuarios);
+escrever($json);
+echo json_encode($usuario);
 
-            $json = json_encode($usuarios);
-            escrever($json);
-            echo json_encode($usuario);
-        
-        
-    
+
+
 ?>
