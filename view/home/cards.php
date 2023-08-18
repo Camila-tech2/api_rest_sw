fca68929a0f30bcfd961288135b972ff7de50a2f
Your Name <you@example.com>
2023-08-18 15:22:31 -0300

    imagens

--- view/home/cards.php
+++ view/home/cards.php
@@ -1,55 +1,55 @@
 
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/get.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">GET</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Exibe a tabela de dados.</p>
+          <a href="../get/view_get.html" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/getid.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">GET for ID</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Busca por ID.</p>
+          <a href="../get_id" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/post.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">POST</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Cadastra um novo produto.</p>
+          <a href="../post/view_post.html" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/patch.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">PATCH</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Edita os dados.</p>
+          <a href="../patch" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/put.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">PUT</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Edita por ID</p>
+          <a href="../put" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/delete.png" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">DELETE</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text"> Deleta por ID.</p>
+          <a href="../delete" class="btn btn-primary">Clique aqui</a>
         </div>
       </div>
 
